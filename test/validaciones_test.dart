140c6510594943dc412b806ddeb96c6e519e4df4
PaolaIsabel <39540572+PaolaIsabel@users.noreply.github.com>
2026-07-13 22:13:46 -0400

    fix: corregir prueba

--- test/validaciones_test.dart
+++ test/validaciones_test.dart
@@ -11,7 +11,7 @@ void main() {
       final resultado = Validaciones.serieValida(serie);
 
       // Assert
-      expect(resultado, false);
+      expect(resultado, true);
     });
 
     test('Rechaza una serie vacía', () {
